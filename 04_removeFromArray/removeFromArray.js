089e3772d1ee3930d390c5e6be30bd89241a6779
Isaac Desktop <86341066+IsaacTanChongYing@users.noreply.github.com>
2024-05-07 18:01:57 +0800

    removefromarray exercise attempted

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) 
+{
+    // The first argument is the array
+    const array = args[0];
 
-};
+    // Remaining arguments are the items to remove
+    const itemsToRemove = args.slice(1);
 
+    // Return a filtered array, excluding any items in `itemsToRemove`
+    return array.filter(item => !itemsToRemove.includes(item));
+}
+removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
