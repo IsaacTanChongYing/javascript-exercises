1af8fd4a54d8a73a83d43140f73c6f58a509a07f
Isaac Desktop <86341066+IsaacTanChongYing@users.noreply.github.com>
2024-06-18 18:08:27 +0800

    exercise 4 fixed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,13 @@
 const removeFromArray = function (array, ...args) 
 {
     // The first argument is the array
-    const array = args[0];
+    const arr = array
 
     // Remaining arguments are the items to remove
-    const itemsToRemove = args.slice(1);
+    const itemsToRemove = args
 
     // Return a filtered array, excluding any items in `itemsToRemove`
-    return array.filter(item => !itemsToRemove.includes(item));
+    return arr.filter(item => !itemsToRemove.includes(item));
 }
 removeFromArray([1, 2, 3, 4], 3);
 
